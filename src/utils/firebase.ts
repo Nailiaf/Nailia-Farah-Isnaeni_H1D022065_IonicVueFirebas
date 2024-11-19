a09742635cc29ad1ca6c6bf20cea26c93470a924
unknown <nailia.isnaeni@mhs.unsoed.ac.id>
2024-11-19 23:35:57 +0700

    first push

---
+++ src/utils/firebase.ts
@@ -0,0 +1,18 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyByAW6OWRpKeZkzjv2ZbZjXxF-SlMSbyGA",
+    authDomain: "vue-firebase-8938c.firebaseapp.com",
+    projectId: "vue-firebase-8938c",
+    storageBucket: "vue-firebase-8938c.firebasestorage.app",
+    messagingSenderId: "589662909436",
+    appId: "1:589662909436:web:7f273a3fc8c5fd9c5d7879"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
